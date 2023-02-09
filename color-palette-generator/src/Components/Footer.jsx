8a0d8dd46eb1c08e8e5af209229398fabfc5f257
Bryan <bryanpool991@hotmail.com>
2023-02-09 12:29:55 -0500

    script del component footer

---
+++ color-palette-generator/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer=()=>{
+    return(
+        <footer>
+            <p>
+            Click to copy individual color • Press “C” to copy palette
+            </p>
+        </footer>
+    )
+}
+export default Footer;
