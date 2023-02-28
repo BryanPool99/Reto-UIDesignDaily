11b552b7953f588ed501c51c545a48920624e4cc
BryanPool99 <bryanpool991@hotmail.com>
2023-02-28 12:31:48 -0500

    script donde se ecnuentra lso datos que se suaron en formarto json

---
+++ chatp-panel/src/data.js
@@ -0,0 +1,98 @@
+import img1 from './images/aiony-haust.jpg';
+import img2 from './images/ali-morshedlou.jpg';
+import img3 from './images/christina-wocintechchat.jpg';
+import img4 from './images/christopher-campbell.jpg';
+import img5 from './images/craig-mckay.jpg';
+import img6 from './images/daniel-lincoln.jpg';
+import img7 from './images/jurica-koletic.jpg';
+import img8 from './images/michael-austin.jpg';
+import img9 from './images/michael-dam.jpg';
+import img10 from './images/sergio-de-paula.jpg';
+import img11 from './images/stefan-stefancik.jpg';
+import img12 from './images/warren-wong.jpg';
+export const data=[
+    {
+        id:1,
+        imgURL:img1,
+        alt:"aiony-haust",
+        name:"Aiony Haust",
+        isActive:true,
+    },
+    {
+        id:2,
+        imgURL:img2,
+        alt:"ali-morshedlou",
+        name:"Ali Morshedlou",
+        isActive:true,
+    },
+    {
+        id:3,
+        imgURL:img3,
+        alt:"christina-wocintechchat",
+        name:"Christina Wocintechchat",
+        isActive:true,
+    },
+    {
+        id:4,
+        imgURL:img4,
+        alt:"christopher-campbell",
+        name:"christopher-campbell",
+        isActive:true,
+    },
+    {
+        id:5,
+        imgURL:img5,
+        alt:"craig-mckay",
+        name:"Craig Mckay",
+        isActive:true,
+    },
+    {
+        id:6,
+        imgURL:img6,
+        alt:"daniel-lincoln",
+        name:"Daniel Lincoln",
+        isActive:true,
+    },
+    {
+        id:7,
+        imgURL:img7,
+        alt:"jurica-koletic",
+        name:"Jurica Koletic",
+        isActive:true,
+    },
+    {
+        id:8,
+        imgURL:img8,
+        alt:"michael-austin",
+        name:"Michael Austin",
+        isActive:true,
+    },
+    {
+        id:9,
+        imgURL:img9,
+        alt:"michael-dam",
+        name:"Michael Dam",
+        isActive:true,
+    },
+    {
+        id:10,
+        imgURL:img10,
+        alt:"sergio-de-paula",
+        name:"Sergio de Paula",
+        isActive:true,
+    },
+    {
+        id:11,
+        imgURL:img11,
+        alt:"stefan-stefancik",
+        name:"Stefan Stefancik",
+        isActive:true,
+    },
+    {
+        id:12,
+        imgURL:img12,
+        alt:"warren-wong",
+        name:"Warren Wong",
+        isActive:true,
+    },
+]
