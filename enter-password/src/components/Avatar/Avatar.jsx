5b44b5de3a4fd26e0c19af50c64b0485eb640ef2
BryanPool99 <bryanpool991@hotmail.com>
2023-02-28 08:20:04 -0500

    mejora de jsx en component avatar

--- enter-password/src/components/Avatar/Avatar.jsx
+++ enter-password/src/components/Avatar/Avatar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import img from '../../Img/matheus-ferrero.jpg';
 const Avatar=()=>{
     return(
-        <section>
-            <img src={img} alt='avatar' style={{width:'5rem'}}/>
+        <section className='sectio-avatar'>
+            <img src={img} alt='avatar'/>
             <div>
                 <p>Business Account</p>
                 <b>Sarah Bills</b>
