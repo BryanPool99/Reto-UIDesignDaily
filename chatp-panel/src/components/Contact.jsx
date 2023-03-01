7f4734a41bf1da6317a9c2ae97d6894cb91a85c0
BryanPool99 <bryanpool991@hotmail.com>
2023-03-01 10:35:06 -0500

    component Contact finalizado

--- chatp-panel/src/components/Contact.jsx
+++ chatp-panel/src/components/Contact.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-const Contact=( {data} )=>{
+const Contact=( { data,searchTerm } )=>{
+    const filteredData = data.filter((contact) =>
+    contact.name.toLowerCase().startsWith(searchTerm.toLowerCase()) // filtra los contactos cuyos nombres incluyen el valor de searchTerm
+    );
     return(
         <ul>
-            {data.map((e)=>
+            {filteredData.map((e)=>
                 <li key={e.id}>
                     <figure>
                         <img src={e.imgURL} alt={e.alt}/>
+                        <div className={e.isActive ? 'online' : ''}></div>
                     </figure>
                     <b>{e.name}</b>
                 </li>
             )}
-         </ul>
+        </ul>
     );
 }
 export default Contact;
