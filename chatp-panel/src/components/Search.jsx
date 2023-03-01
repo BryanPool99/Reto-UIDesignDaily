71c3e78efe6428852c6837cbae17b98a3dc0f4de
BryanPool99 <bryanpool991@hotmail.com>
2023-03-01 10:36:21 -0500

    component Search finalizado

--- chatp-panel/src/components/Search.jsx
+++ chatp-panel/src/components/Search.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { MdSearch } from "react-icons/md";
-const Search=()=>{
+const Search=({ setSearchTerm })=>{
+    
     return(
         <section className='container-search'>
             <aside>
                 <button>
                     <MdSearch/>
                 </button>
-                <input type={'text'} placeholder={'Search'}/>
+                <input type={'text'} placeholder={'Search'} 
+                    onChange={(e)=>{setSearchTerm(e.target.value)}}
+                />
             </aside>
         </section>
     );
