94665be4e3256302b76687b9d26bbcf0f0699713
BryanPool99 <bryanpool991@hotmail.com>
2023-03-01 10:35:42 -0500

    component Header finalizado

--- chatp-panel/src/components/Header.jsx
+++ chatp-panel/src/components/Header.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { MdOutlineModeEditOutline,MdSettings } from "react-icons/md";
 import { data } from '../data';
 const Header=()=>{
+    const cantOnline=()=>{
+        let val=data.filter(e=>e.isActive);
+        return val.length;
+    }
     return(
         <section className='Header'>
             <div>
                 <h1>Chat</h1>
-                <p>{data.length} online</p>
+                <p>{cantOnline()} online</p>
             </div>
             <div className='icons'>
                 <button>
                     <MdOutlineModeEditOutline/>
                 </button>
-                <button>
+                <button className='setting'>
                     <MdSettings/>
                 </button>
             </div>
